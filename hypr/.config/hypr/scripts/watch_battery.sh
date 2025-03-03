637bfdfe353a9406dd49a0bee664df2580f4671f
Rodrigo <rodrigo.coffani@gmail.com>
2025-03-03 16:37:48 +0100

    update scripts

--- hypr/.config/hypr/scripts/watch_battery.sh
+++ hypr/.config/hypr/scripts/watch_battery.sh
@@ -3,8 +3,8 @@
 while [[ true ]]; do
   battery=$(upower -i $(upower -e | grep BAT) | grep --color=never -E percentage | xargs | cut -d' ' -f2 | sed s/%//)
   if [[ $battery -ge 80 ]]; then
-    notify-send "Battery above 80%" "Stop charging!"
-    /home/rcoff/.config/swaync/Sound.sh
+    # notify-send "Battery above 80%" "Stop charging!"
+    # /home/rcoff/.config/swaync/Sound.sh
   elif [[ $battery -le 20 ]]; then
     notify-send "Battery below 20%" "Please charge!"
     /home/rcoff/.config/swaync/Sound.sh
